67e3c3603e4458cb3faacbb93f8e055ade1eaff1
Theo Patt <theo@theopatt.com>
2019-01-31 15:53:27 -0800

    announcements: + slack integration

--- src/js/announcements.jsx
+++ src/js/announcements.jsx
@@ -1,16 +1,48 @@
 import React from 'react';
+import { emojify } from 'react-emojione';
+import Remarkable from "remarkable";
 
-function Announcements() {
-  return (
-    <div id="announcements">
-      <h1 className="section-heading">Announcements</h1>
-      <hr />
-      <div className="announcement">
-        <h1>Hello</h1>
-        <p>This is a paragraph.</p>
+class Announcements extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      announcements: []
+    };
+
+    this.md = new Remarkable({
+      breaks: true,
+      linkify: true,
+    });
+  }
+
+  componentWillMount() {
+    fetch('https://api.treehacks.com/announcements').then(r => r.json()).then(announcements => {
+      this.setState({ announcements });
+    });
+  }
+
+  render() {
+    const { announcements } = this.state;
+    return (
+      <div id="announcements">
+        <h1 className="section-heading">Announcements</h1>
+        <hr />
+        {announcements.length ?
+          announcements.map(({ text, ts }) => (
+            <div key={ts} className="announcement">
+              <div class="message" dangerouslySetInnerHTML={ { __html: this.md.render(emojify(text, { output: 'unicode' })) } } />
+              <p class="ts">{new Date(ts * 1000).toLocaleString()}</p>
+            </div>
+          ))
+        :
+          <div class="loading">
+            <p>Loading...</p>
+          </div>
+        }
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Announcements;
