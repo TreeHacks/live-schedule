11e205a295e220de3e9c09e8621e7ebc3af86ee7
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-02-05 10:42:22 -0800

    feat(apis): organize data structure by company

--- src/js/apis/Apis.jsx
+++ src/js/apis/Apis.jsx
@@ -6,15 +6,18 @@ export default () => (<div class="bootstrap container">
         <h1 className="col-12 my-4 text-center">API Directory</h1>
     </div>
     <div className="card-columns">
-        {data.map(api =>
-            <div className="card api-item p-4" key={api.title}>
-                <h3>{api.title}</h3>
-                <p>{api.description}</p>
-                {api.links.map(link =>
-                    <a target="_blank" href={link.url}>
-                        <button className="green-button">{link.title || link.url}</button>
-                    </a>
-                )}
-            </div>)}
+        {data.map(company =>
+            company.apis.map(api =>
+                <div className="card api-item p-4" key={api.title}>
+                    <h3>{api.title}</h3>
+                    <p>{api.description}</p>
+                    <p><a href={company.slack}>#slack channel</a></p>
+                    {api.links.map(link =>
+                        <a target="_blank" href={link.url}>
+                            <button className="green-button">{link.title || link.url}</button>
+                        </a>
+                    )}
+                </div>)
+        )}
     </div>
 </div>);
