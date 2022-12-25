2eec10ab6c222b64df04b5491caf76c9df8baaa0
Joseph Semrai <josephsemrai@gmail.com>
2022-12-24 20:05:53 -0500

    Update fonts and colors

--- src/js/header.jsx
+++ src/js/header.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import * as whiteLeavesLogo from '../svg/whiteLeavesLogo.svg';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faInstagram, faFacebookF, faTwitter} from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import * as whiteLeavesLogo from "../svg/whiteLeavesLogo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faFacebookF,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 // import Timer from './timer.jsx';
 
 function Header() {
-  return(
+  return (
     <nav id="header">
-      <ul className="container" style={{background: '#15766A'}}>
+      <ul className="container" style={{ background: "#0CB08A" }}>
         <li id="header-logo">
           <a href="/">
-            <img src={whiteLeavesLogo} alt="treehacks small logo"/>
+            <img src={whiteLeavesLogo} alt="treehacks small logo" />
             <span className="logo-text-tree">tree</span>
             <span className="logo-text-hacks">hacks</span>
             <span className="logo-text-live">live</span>
@@ -23,11 +27,29 @@ function Header() {
 }
 
 function SocialMedia() {
-  return(
+  return (
     <div id="social-media">
-      <a style={{background: 'white'}} href="https://www.instagram.com/stanfordtreehacks" target="_blank"><FontAwesomeIcon style={{color: '#15766A'}} icon={faInstagram} /></a>
-      <a style={{background: 'white'}} href="https://www.facebook.com/treehacks/" target="_blank"><FontAwesomeIcon style={{color: '#15766A'}} icon={faFacebookF} /></a>
-      <a style={{background: 'white'}} href="https://twitter.com/hackwithtrees" target="_blank"><FontAwesomeIcon style={{color: '#15766A'}} icon={faTwitter} /></a>
+      <a
+        style={{ background: "white" }}
+        href="https://www.instagram.com/stanfordtreehacks"
+        target="_blank"
+      >
+        <FontAwesomeIcon style={{ color: "#105E54" }} icon={faInstagram} />
+      </a>
+      <a
+        style={{ background: "white" }}
+        href="https://www.facebook.com/treehacks/"
+        target="_blank"
+      >
+        <FontAwesomeIcon style={{ color: "#105E54" }} icon={faFacebookF} />
+      </a>
+      <a
+        style={{ background: "white" }}
+        href="https://twitter.com/hackwithtrees"
+        target="_blank"
+      >
+        <FontAwesomeIcon style={{ color: "#105E54" }} icon={faTwitter} />
+      </a>
     </div>
   );
 }
