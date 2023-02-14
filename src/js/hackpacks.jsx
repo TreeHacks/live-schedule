d3e2e19387d09277466c0e7f97613e048a00fd10
sathviknallamalli <sathviknallamalli@gmail.com>
2023-02-13 18:29:19 -0800

    prehackathon workshops

--- src/js/hackpacks.jsx
+++ src/js/hackpacks.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import {hackpacks} from './content.json';
+import React from "react";
+import { hackpacks, preHackathonWorkshops } from "./content.json";
 
 function Hackpacks() {
   var content = [];
   for (var i = 0; i < hackpacks.length; i++) {
-    content.push(HackPack(hackpacks[i].name, hackpacks[i].link))
+    content.push(HackPack(hackpacks[i].name, hackpacks[i].link));
+  }
+
+  var content2 = [];
+  for (var i = 0; i < preHackathonWorkshops.length; i++) {
+    content2.push(
+      HackPack(preHackathonWorkshops[i].name, preHackathonWorkshops[i].link)
+    );
   }
   return (
     <div id="hackpacks" className="section">
       <h1 className="section-heading">Hackpacks</h1>
       <hr />
-      <div className="container">
-        {content}
-      </div>
+      <div className="container">{content}</div>
+      <br />
+      <h1 className="section-heading">Pre-Hackathon Workshops</h1>
+      <hr />
+      <div className="container">{content2}</div>
     </div>
   );
 }
 
 function HackPack(name, link) {
-  return(
+  return (
     <a href={link} target="_blank">
       <button className="main-button inverted-button">{name}</button>
     </a>
