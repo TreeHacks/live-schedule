88943700db81d81ffa3924d39533f9d539994cfb
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2024-02-07 17:39:50 -0800

    fix intersystems desc

--- src/js/prizes/Prizes.jsx
+++ src/js/prizes/Prizes.jsx
@@ -267,9 +267,9 @@ export default class extends React.Component {
         logo: Codegen,
       },
       {
-        name: "Best Use of GenAI using InterSystems IRIS Vector Search (1st)",
+        name: "Best Use of GenAI using InterSystems IRIS Vector Search",
         description:
-          "InterSystems IRIS Vector Search is our new vector store feature within the IRIS Data Platform! But we're not a "vector database" just like we're not a "string database" or "integer database" 😉 -- embedding vectors are just another SQL datatype for us!  Build your groundbreaking solution on InterSystems IRIS data platform, which gives you the tools to seamlessly integrate GenAI, tabular AutoML, data interoperability, using simple SQL or Python LangChain/Llamaindex connectors! The most innovative solution that leverages GenAI using InterSystems IRIS will win cash prizes!",
+          "Build your groundbreaking solution on InterSystems IRIS data platform, which gives you the tools to seamlessly integrate GenAI, tabular AutoML, data interoperability, using simple SQL or Python LangChain/Llamaindex connectors! The most innovative solution that leverages GenAI using InterSystems IRIS will win cash prizes!",
         contents: {
           1: {
             name: "$2,000 Cash",
